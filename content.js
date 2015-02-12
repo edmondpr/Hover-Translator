f60eecb9183235f90b36b9f99ea0bbd3466b7bf0
pbraunstein <pbraunstein12@gmail.com>
2015-02-12 08:38:44 -0500

    Reimplemented by using hover tooltips, currently works, but breaks links, which is no goow

--- content.js
+++ content.js
@@ -14,29 +14,28 @@ console.log("In the content script");
 
 // Adapted from accepted solution on: 
 //http://stackoverflow.com/questions/2444430/how-to-get-a-word-under-cursor-using-javascript
-$('p').each(function() {
+$('p, h1, h2, h3').each(function() {
     var $this = $(this);
+    console.log(this.nodeName);
     $this.html($this.text().replace(/\b(\w+)\b/g, "<span>$1</span>"));
 });
 
 // bind to each span
 var SAVED;
-var APIKey = "SPOOF"
-$('p span').mousedown(
-    function() {
-    	var req = new XMLHttpRequest();
-    	var url = "https://translate.yandex.net/api/v1.5/tr/translate?key=" + APIKey + "&lang=de-en&text=" + $(this).html();
-    	req.open("GET", url, false);
-    	req.setRequestHeader('Content-Type', 'text/xml');
-    	req.send();
-    	xmlResponse = req.responseXML;
-    	var newText = xmlResponse.childNodes['0'].textContent;
-    	SAVED = $(this).html();
-    	$(this).html("<span>" + newText + "</span>");
-    }
- );
-$('p span').mouseup(
-	function() {
-		$(this).html("<span>" + SAVED + "</span>");
-	}
-);
+var APIKey = "SPOFF"
+
+$('p span, h1 span, h2 span, h3 span').hover(function() {
+        var req = new XMLHttpRequest();
+        var url = "https://translate.yandex.net/api/v1.5/tr/translate?key=" + APIKey + "&lang=de-en&text=" + $(this).html();
+        req.open("GET", url, false);
+        req.setRequestHeader('Content-Type', 'text/xml');
+        req.send();
+        xmlResponse = req.responseXML;
+        var newText = xmlResponse.childNodes['0'].textContent;
+
+        // Only render tooltip if the text is different
+        if (newText != $(this).html()) {
+            $(this).attr('title', newText);
+        }
+        
+    });
--- popup.js
+++ popup.js
@@ -1,1 +1,10 @@
+console.log("HELLO WORLD");
 
+function wasClicked() {
+	console.log("I was cliked!!");
+	var newURL = "https://translate.yandex.com/"
+	chrome.tabs.create({url : newURL});
+}
+
+var linkToYandex = document.getElementById("Yandex");
+linkToYandex.addEventListener("click", wasClicked, false);
