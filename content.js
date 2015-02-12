beaae5981a04920a51597f9b392eef0133df0756
pbraunstein <pbraunstein12@gmail.com>
2015-02-11 20:09:48 -0500

    Added code to actully change HTML on a website and it works.

--- content.js
+++ content.js
@@ -1,2 +1,18 @@
-document.body.style.background = 'red';
+//document.body.style.background = 'red';
 console.log("In the content script");
+
+// var newElList = document.getElementsByClassName("story-heading");
+// for (var i = 0; i < newElList.length; i++) {
+// 	newElList[i].innerHTML = "Phil is the BEST";
+// }
+
+// var anotherList = document.getElementsByTagName("li");
+
+// for (var i = 0; i < anotherList.length; i++) {
+	// anotherList[i].innerHTML = "Phil";
+// }
+
+
+
+
+//console.log(newEl);
