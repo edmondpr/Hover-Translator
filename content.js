fae15768874cb96f01dfcaf798d866d17e448be9
pbraunstein <pbraunstein12@gmail.com>
2015-02-17 14:45:19 -0500

    Made xmhtl requests asynchrnous as recommended

--- content.js
+++ content.js
@@ -18,20 +18,27 @@ $('p, h1, h2, h3').each(function() {
     $(this).html($(this).text().replace(/\b(\S+)\b/g, "<span>$1</span>"));
 });
 
-
 $('p span, h1 span, h2 span, h3 span').hover(function(){
     // translate
-    console.log($(this).html());
+    var currentElement = $(this);
     var reqTrans = new XMLHttpRequest();
     var urlTrans = "https://translate.yandex.net/api/v1.5/tr/translate?key=" + APIKey + "&lang=de-en&text=" + $(this).html();
-    reqTrans.open("GET", urlTrans, false);
+    reqTrans.onreadystatechange = function() {
+        //console.log(reqTrans.status);
+        if (reqTrans.readyState == 4) {
+            console.log("good request");
+
+            var xmlResponseTrans = reqTrans.responseXML;
+            var newText = xmlResponseTrans.childNodes[0].textContent;
+
+            // Only render tooltip if the text is different
+            if (newText != currentElement.html()) {
+                currentElement.attr('title', newText);
+            }
+        }
+    }
+    reqTrans.open("GET", urlTrans, true);
     reqTrans.setRequestHeader('Content-Type', 'text/xml');
     reqTrans.send();
-    var xmlResponseTrans = reqTrans.responseXML;
-    var newText = xmlResponseTrans.childNodes[0].textContent;
 
-    // Only render tooltip if the text is different
-    if (newText != $(this).html()) {
-        $(this).attr('title', newText);
-    }
 });
